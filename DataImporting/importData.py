b3909ec9f950f0a17dd25b6ec8933d1408d51a84
GeneralZYQ <zhangyuanqing.generalzyq@gmail.com>
2019-12-18 13:15:52 +0100

    raw data

--- DataImporting/importData.py
+++ DataImporting/importData.py
@@ -1,8 +1,15 @@
 
-#Produce the input features
 
+#Import libraries
 import math 
 
+import pandas as pd # for using pandas daraframe
+import numpy as np # for som math operations
+# from sklearn.preprocessing import StandardScaler # for standardizing the Data
+# from sklearn.decomposition import PCA # for PCA calculation
+# import matplotlib.pyplot as plt # for plotting
+
+#Produce the input features
 fotraning = open('ae.train', 'r+')
 allmanFeatures = []
 oneManFeatures = []
@@ -27,15 +34,15 @@ while True:
 
 
 # print (len(allmanFeatures[1]))
-
+#Produce the training labels
 trainingLabels = []
 for i in range(0,270):
     featureIndex = math.ceil(i / 270.0)
     trainingLabels.append(featureIndex)
 
 
-
-fotesting = open('ae.train', 'r+')
+#Produce the testing features
+fotesting = open('ae.test', 'r+')
 alltestFeatures = []
 oneTestFeatures = []
 while True:
@@ -51,14 +58,19 @@ while True:
     if len(components) == 13:
         del components[-1]
     
-    if float(components[0]) != 1.0:
-        oneTestFeatures.append(components)
-    else:
-        alltestFeatures.append(oneTestFeatures);
-        oneTestFeatures = [];
+    print (components)
+    if len(components) > 0:
+        if float(components[0]) != 1.0:
+            oneTestFeatures.append(components)
+        else:
+            alltestFeatures.append(oneTestFeatures);
+            oneTestFeatures = [];
 
+print (len(alltestFeatures))
+#produce the testing labels
 testingLabels = []
 blockLengthes = [31, 35, 88, 44, 29, 24, 40, 50, 29];
+
 
 total = 0
 for j in range(0,len(blockLengthes)):
@@ -66,7 +78,7 @@ for j in range(0,len(blockLengthes)):
     for k in range(total, total + num):
         testingLabels.append(j + 1);
 
-print (len(testingLabels))
+# print (len(testingLabels))
 
     
 
