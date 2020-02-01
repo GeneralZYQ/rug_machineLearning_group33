c1abfab0d4200bad5a0731d35a8f7676263f797b
GeneralZYQ <zhangyuanqing.generalzyq@gmail.com>
2020-02-01 13:07:47 +0100

    applied pca interpolation with 5 points

--- Preprocessed/PCAAfterResampling/ResamplingThenPCA.py
+++ Preprocessed/PCAAfterResampling/ResamplingThenPCA.py
@@ -8,18 +8,24 @@ from sklearn.preprocessing import StandardScaler # for standardizing the Data
 from sklearn.decomposition import PCA # for PCA calculation
 import matplotlib.pyplot as plt # for plotting
 
-TestingDataFrame = pd.read_csv('train_no_pca.csv', header=None) 
+TrainingDataFrame = pd.read_csv('train_no_pca.csv', header=None) 
+TestingDataFrame = pd.read_csv('vanilla_interpolation_test5.csv')
 
+TrainingValues = TrainingDataFrame.values
 TestingValues = TestingDataFrame.values
 
 sc = StandardScaler() # creating a StandardScaler object
-X_std = sc.fit_transform(TestingValues) # standardizing the data
+X_std = sc.fit_transform(TrainingValues) # standardizing the data
+Test_std = sc.fit_transform(TestingValues)
 
 pca = PCA(n_components = 0.95)
 X_pca = pca.fit_transform(X_std)
+X_pca_test = pca.fit_transform(Test_std)
 
 principalDf = pd.DataFrame(data = X_pca)
-print(type(principalDf))
+principalTestDF = pd.DataFrame(data=X_pca_test)
+print(pca.explained_variance_ratio_)
 principalDf.to_csv('pcaed_trainingData.csv', header=False)
+principalTestDF.to_csv('vanilla_interpolation_test5_pca.csv')
 print('success!')
 
