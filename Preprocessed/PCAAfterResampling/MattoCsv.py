f1c346a314acf3cdbc42748b2a672bcb585354b3
GeneralZYQ <zhangyuanqing.generalzyq@gmail.com>
2020-02-02 12:38:19 +0100

    trying to try some methid

--- Preprocessed/PCAAfterResampling/MattoCsv.py
+++ Preprocessed/PCAAfterResampling/MattoCsv.py
@@ -1,8 +1,8 @@
 #This is the file used to transfer the mat to .csv
 import scipy.io
 import numpy as np
-data = scipy.io.loadmat("vanilla_interpolation_test5.mat")
+data = scipy.io.loadmat("vanilla_interpolation_train5.mat")
 
 for i in data:
     if '__' not in i and 'readme' not in i:
-          np.savetxt(("vanilla_interpolation_test5.csv"),data[i],delimiter=',')
+          np.savetxt(("vanilla_interpolation_train5.csv"),data[i],delimiter=',')
--- Preprocessed/PCAAfterResampling/ResamplingThenPCA.py
+++ Preprocessed/PCAAfterResampling/ResamplingThenPCA.py
@@ -8,8 +8,8 @@ from sklearn.preprocessing import StandardScaler # for standardizing the Data
 from sklearn.decomposition import PCA # for PCA calculation
 import matplotlib.pyplot as plt # for plotting
 
-TrainingDataFrame = pd.read_csv('train_no_pca.csv', header=None) 
-TestingDataFrame = pd.read_csv('vanilla_interpolation_test5.csv')
+TrainingDataFrame = pd.read_csv('vanilla_interpolation_train5.csv', header=None) 
+TestingDataFrame = pd.read_csv('vanilla_interpolation_test5.csv', header=None)
 
 TrainingValues = TrainingDataFrame.values
 TestingValues = TestingDataFrame.values
@@ -18,14 +18,31 @@ sc = StandardScaler() # creating a StandardScaler object
 X_std = sc.fit_transform(TrainingValues) # standardizing the data
 Test_std = sc.fit_transform(TestingValues)
 
-pca = PCA(n_components = 0.95)
+pca = PCA(n_components = 0.99)
 X_pca = pca.fit_transform(X_std)
 X_pca_test = pca.fit_transform(Test_std)
 
 principalDf = pd.DataFrame(data = X_pca)
 principalTestDF = pd.DataFrame(data=X_pca_test)
-print(pca.explained_variance_ratio_)
-principalDf.to_csv('pcaed_trainingData.csv', header=False)
-principalTestDF.to_csv('vanilla_interpolation_test5_pca.csv')
-print('success!')
+
+n_pcs= pca.n_components_
+most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pcs)]
+initial_feature_names = TrainingDataFrame.columns
+# get the most important feature names
+most_important_names = [initial_feature_names[most_important[i]] for i in range(n_pcs)]
+print(most_important_names)
+most_important_names = list(set(most_important_names))
+
+
+most_important_training_df = TrainingDataFrame.iloc[:,most_important_names]
+most_important_training_df.to_csv('vanilla_most_important_training_features_099.csv', header=False)
+most_important_testing_df = TestingDataFrame.iloc[:,most_important_names]
+most_important_testing_df.to_csv('vanilla_most_important_testing_features_099.csv', header=False)
+
+
+# print(pca.explained_variance_ratio_)
+# print(pca.components_)
+# principalDf.to_csv('vanilla_interpolation_train5_pca_1.csv', header=False)
+# principalTestDF.to_csv('vanilla_interpolation_test5_pca_1.csv',header = False)
+# print('success!')
 
